2f41cd5ea8d607d9edc651e6df999ed8d1a0faff
Sriteja Chavva <sritejachavva@gmail.com>
2024-01-16 22:12:26 -0800

    add header

---
+++ myscript.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read_it) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read_it = read_it;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
