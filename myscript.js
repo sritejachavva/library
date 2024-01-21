fb9ab98ee4734f9cdb426477e684202616176a6e
Sriteja Chavva <sritejachavva@gmail.com>
2024-01-20 17:35:03 -0800

    fix button

--- myscript.js
+++ myscript.js
@@ -1,13 +1,34 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read_it) {
+
+let add_book_button = document.body.querySelector("button.add_book");
+let bookname = document.body.querySelector("#bookname");
+let pages = document.body.querySelector("#pages");
+let readit = document.body.querySelector("#readit");
+
+
+let list_of_books = [];
+
+
+function Book(title, pages, read_it) {
   this.title = title;
-  this.author = author;
   this.pages = pages;
   this.read_it = read_it;
-  
+
+}
+
+function readInputValues(){
+
+
+
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    
+    console.log(bookname.value);
+    return false;
 }
+
+
+
+//add_book_button.addEventListener('click', addBookToLibrary);
